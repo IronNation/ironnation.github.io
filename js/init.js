f0247dc6d8af1af444f9006ccadb82d6974374db
Brian Dorton <dorton@gmail.com>
2016-12-14 23:59:05 -0600

    adds jquery post to form

--- js/init.js
+++ js/init.js
@@ -3,6 +3,22 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.modal').modal();
+
+    $("#signup").click(function(){
+      var first_name = document.getElementById("first_name").value;
+      var last_name = document.getElementById("last_name").value;
+      var email = document.getElementById("email").value;
+
+    $.post("http://localhost:3000/users.json",
+     {
+        first_name: first_name,
+        last_name: last_name,
+        email: email
+    });
+});
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
